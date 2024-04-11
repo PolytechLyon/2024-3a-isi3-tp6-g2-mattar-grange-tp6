01a5e06f40f95a02301d3addf76959b0c7c1cbaa
Omar Mattar <omar-mazen-mostafa-h.mattar@etu.univ-lyon1.fr>
2024-04-11 10:03:55 +0200

    Q8

--- src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
+++ src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
@@ -3,6 +3,7 @@ package fr.polytech.sim.cycling;
 import fr.polytech.sim.Simulation;
 import fr.polytech.sim.log.Logger;
 import fr.polytech.sim.log.LoggerFactory;
+import fr.polytech.sim.utils.Context;
 
 
 /**
@@ -12,12 +13,13 @@ public class BikeSimulator implements Simulation {
     private final Logger logger = LoggerFactory.CreateLogger("BikeSimulator");
 
     public void run() {
-        Bike bike = new SimpleBike();
-        Bike bikeParents = new TagAlongBike();
+        //Bike bike = new SimpleBike();
+        //Bike bike = new TagAlongBike();
+
+        Bike bike = Context.inject(Bike.class);
+
         this.logger.log("Bike's speed %.2f Km/h.", bike.getVelocity());
         this.logger.log("Bike's mass %.2f Kg.", bike.getMass());
 
-        this.logger.log("TagAlongBike's speed %.2f Km/h.", bikeParents.getVelocity());
-        this.logger.log("TagAlongBike's mass %.2f Kg.", bikeParents.getMass());
     }
 }
